db5dd72f3724bd5a554609c30687f7a1d7054002
Bartosz Dutka <dutkabar@gmail.com>
2023-12-04 16:35:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,31 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 from imgclf.config.settings import settings
+from imgclf.common.logger import logger
 from imgclf.data import load_data
 from imgclf.models.networks import NeuralNetwork
+from imgclf.models import model_manager
 
 
 def main():
-    ds_train, ds_val, ds_test = load_data.run_ds_loading()
+    model_path = 'artifacts/models'
+    model_file = 'imgclf_1.keras'
+    input_shape = (28, 28, 3)
+    num_classes = 10
+    batch_size = 128
+    img_size = (28, 28)
+    validation_split_seed = 42
+    shuffle_seed = 42
+    data_dir = r"artifacts/datasets/mnist"
+    data_dir_labeled = r"artifacts/datasets/mnist_split"
+
+    ds_train, ds_val, ds_test = load_data.run_ds_loading(data_dir_labeled, img_size, batch_size, validation_split_seed, shuffle_seed)
+
     neural_network = NeuralNetwork()
-    model = neural_network.conv_net_1(input_shape=(28, 28, 3), num_classes=10)
+    model = neural_network.conv_net_1(input_shape=input_shape, num_classes=num_classes)
     callbacks = neural_network.get_callbacks('default')
     cb_history = neural_network.train_model(model=model, ds_train=ds_train, ds_val=ds_val, num_epochs=settings.models.conv_1.epochs, callbacks=callbacks)
+    model_manager.save_model(model, model_path, model_file)
 
 
 if __name__ == "__main__":
